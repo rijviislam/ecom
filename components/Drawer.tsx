f182c68bb05686e7eba6f61bb1fc3a83eea5dd8a
rijviislam <rijviislam.github@gmail.com>
2026-08-28 15:40:41 +0600

    fix: drawer issue has been fix

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -34,7 +34,7 @@ export default function Drawer({
     <>
       <div
         onClick={onClose}
-        className={`fixed left-0 right-0 top-18 md:top-20 bottom-0 z-40 bg-ink/40 backdrop-blur-md bg-[#ece0de]/10 transition-opacity duration-300 ease-in-out ${
+        className={`fixed inset-0 z-40 bg-ink/40 backdrop-blur-md bg-[#ece0de]/10 transition-opacity duration-300 ease-in-out ${
           isOpen
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
